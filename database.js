cd9a070e12b685fcd7fcbf9dbc9aa4b69e67c356
lykmapipo <lallyelias87@gmail.com>
2019-06-12 19:55:36 +0300

    feat: add data models

---
+++ database.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* dependencies */
+const { connect } = require('@lykmapipo/mongoose-common');
+const { Predefine } = require('@lykmapipo/predefine');
+const { Permission } = require('@lykmapipo/permission');
+const { Feature } = require('@codetanzania/emis-feature');
+const { Role } = require('@codetanzania/emis-role');
+const { Party } = require('@codetanzania/emis-stakeholder');
+
+/* export models */
+module.exports = exports = {
+  connect,
+  Predefine,
+  Permission,
+  Feature,
+  Role,
+  Party,
+};
